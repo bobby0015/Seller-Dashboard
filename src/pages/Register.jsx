c510c7333f8aa53204f9eef5f47d931705de075a
Divyam Chauhan <contact.dcdeveloper@gmail.com>
2025-01-16 17:53:53 +0530

    first half of the register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      Register
-    </div>
-  )
-}
+    <section className="flex justify-center items-center h-screen">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Signup</CardTitle>
+          <CardDescription>
+            Create a new Seller account for your business.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="flex gap-2">
+            <div className="grid gap-2">
+              <Label htmlFor="firstName">First Name</Label>
+              <Input id="firstName" type="text" placeholder="John" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="lastName">Last Name</Label>
+              <Input id="lastName" type="text" placeholder="Doe" required />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" required />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="confrimPassword">Confirm Password</Label>
+            <Input id="confirmPassword" type="text" placeholder="Retype Password" required />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="w-full">
+            <Button className="w-full">
+              <span className="ml-2">Sign in</span>
+            </Button>
 
-export default Register
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link to={"/login"} className="underline">
+                Log In
+              </Link>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+    </section>
+  );
+};
+
+export default Register;
