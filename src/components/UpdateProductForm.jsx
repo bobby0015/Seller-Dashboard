21ac3cef2792b8f7a3e8714d75ef79e7c203d809
Divyam Chauhan <contact.dcdeveloper@gmail.com>
2025-01-19 09:40:07 +0530

    basic funtionality of update product done lil bit issues

--- src/components/UpdateProductForm.jsx
+++ src/components/UpdateProductForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AlertDialogAction,
   AlertDialogCancel,
@@ -18,17 +18,57 @@ import {
 } from "./ui/card";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import { updateProduct, uploadImage } from "@/http/api";
 
 const UpdateProductForm = ({ productId }) => {
+  const [updateProductData, setUpdateProductData] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const otherMutation = useMutation({
+    mutationFn: updateProduct,
+    onSuccess: (response) => {
+      console.log(response);
+    },
+  });
+
+  const mutation = useMutation({
+    mutationFn: uploadImage,
+    onSuccess: (response) => {
+      if (!response.data.image_url) {
+        return;
+      }
+      const data = {
+        ...updateProductData,
+        image: response.data.image_url,
+        productId,
+      };
+      console.log(data);
+      otherMutation.mutate(data);
+    },
+  });
+
+  const onSubmit = (data) => {
+    if (data.file === undefined) {
+      const newdata = { ...data, productId,image:"" };
+      console.log(newdata);
+      otherMutation.mutate(newdata);
+      return;
+    } else {
+      const formData = new FormData();
+      formData.append("file", data.file[0]);
+      mutation.mutate(formData);
+      setUpdateProductData(data);
+    }
+  };
+
   return (
     <AlertDialogContent>
       <AlertDialogHeader>
@@ -42,48 +82,77 @@ const UpdateProductForm = ({ productId }) => {
         <AlertDialogDescription>
           <Card className="w-full py-2">
             <CardContent>
-              <form>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="grid w-full items-center gap-4">
                   <div className="grid grid-cols-2 gap-2">
                     <div>
                       <Label htmlFor="name">Name</Label>
-                      <Input id="name" placeholder="Name of your project" />
+                      <Input
+                        {...register("name")}
+                        id="name"
+                        s
+                        placeholder="Name of your project"
+                      />
                     </div>
                     <div className="">
                       <Label htmlFor="product picture">Product Image</Label>
-                      <Input type="file" id="file" accept="image/*" />
+                      <Input
+                        {...register("image")}
+                        type="file"
+                        id="file"
+                        accept="image/*"
+                      />
                     </div>
                   </div>
                   <div className="grid gap-2">
                     <Label htmlFor="product description">
                       Product Description
                     </Label>
-                    <Textarea placeholder="Describe your product here..."></Textarea>
+                    <Textarea
+                      {...register("description")}
+                      placeholder="Describe your product here..."
+                    ></Textarea>
                   </div>
                   <div className="grid grid-cols-4 gap-2">
                     <div>
                       <Label htmlFor="product warranty">Warranty</Label>
-                      <Input type="number" placeholder="24 months" />
+                      <Input
+                        {...register("warranty")}
+                        type="number"
+                        placeholder="24 months"
+                      />
                     </div>
                     <div>
                       <Label htmlFor="product stock">Stock</Label>
-                      <Input type="number" placeholder="256 units" />
+                      <Input
+                        {...register("stock")}
+                        type="number"
+                        placeholder="256 units"
+                      />
                     </div>
                     <div>
                       <Label htmlFor="product price">Price (in ₹ Rs.)</Label>
-                      <Input type="number" placeholder="₹ 25,600" />
+                      <Input
+                        {...register("price")}
+                        type="number"
+                        placeholder="₹ 25,600"
+                      />
                     </div>
                     <div>
                       <Label htmlFor="product discount">Discount (in %)</Label>
-                      <Input type="number" placeholder="discount" />
+                      <Input
+                        {...register("discount")}
+                        type="number"
+                        placeholder="discount"
+                      />
                     </div>
                   </div>
                 </div>
+                <div className="mt-6">
+                  <Button>Update</Button>
+                </div>
               </form>
             </CardContent>
-            <CardFooter className="flex">
-              <Button>Update</Button>
-            </CardFooter>
           </Card>
         </AlertDialogDescription>
       </AlertDialogHeader>
--- src/http/api.js
+++ src/http/api.js
@@ -16,5 +16,6 @@ export const login = async (data) => api.post('/login',data);
 export const register = async (data) => api.post('/register',data);
 
 export const uploadProduct = async (data) => api.post('/product/create',data);
+export const updateProduct = async (data) => api.patch('/product/update',data);
 
 export const uploadImage = async (data) => fileApi.post('/product/image-upload',data);
